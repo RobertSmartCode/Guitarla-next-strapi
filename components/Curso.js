7ca9d5515a8900e9dd06f6e2b94e9d59a86ed0a5
Robert Smart <roca1388@gmail.com>
2022-01-15 14:02:13 -0300

    course finished

--- components/Curso.js
+++ components/Curso.js
@@ -1,18 +1,33 @@
-// import Imagen from 'next/imagen'
+import styles from '../styles/Curso.module.css'
 
 const Curso = ({cursos}) => {
 
-    const {titulo, contenido, imagenes}= cursos
+    const {titulo, contenido, imagen}= cursos
     return (
-        <section>
-            <div className="contenedor" >
-                <div>
-                    <h2>{titulo}</h2>
-                    <p>{contenido}</p>
-                    <a href="#" >Mas Informacion</a>
+        <section >
+            <div className={`contenedor ${styles.grid}`} >
+                <div className={styles.contenido}>
+                    <h2 className='heading' >{titulo}</h2>
+                    <p className={styles.texto} >{contenido}</p>
+                    <a className={styles.enlace} href="#" >Mas Informacion</a>
                 </div>
 
             </div>
+
+            <style jsx >{`
+
+            section{
+                padding: 10rem 0;
+                margin-top: 10rem;
+                background-image: linear-gradient(to right, rgb(0 0 0 / .65), rgb(0 0 0 / .7)),
+                url(${imagen.url}) ;
+                background-size: cover;
+                background-position: 50%;
+            }
+            
+            `}
+
+            </style>
           
         </section>
     )
